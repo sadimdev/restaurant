a809c90595365b77a51f43280defb1bd4b527ebf
Sadim Mali <sadimmalakar77@gmail.com>
2024-03-28 15:31:20 +0545

    refactor: Remove overall padding with px

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="h-12 md:h-24  p-4 lg:p-20 xl:p-40 flex items-center justify-between text-red-500">
+    <div className="h-12 md:h-24  p-4 lg:px-20 xl:px-40 flex items-center justify-between text-red-500">
       <Link href="/" className="font-bold text-xl uppercase">Massimo</Link>
       <p>@ ALL RIGHTS RESERVED.</p>
     </div>
